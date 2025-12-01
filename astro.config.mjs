2100f1af1fcee351ea01efa7b8bab948556a1895
EstebanDev <guachichullcaesteban@gmail.com>
2025-12-01 11:10:15 -0500

    Eliminacion de las dependencias de react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
@@ -12,6 +9,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
 });
